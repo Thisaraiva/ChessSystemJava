8f75e887c979db5860c2baaf908d44a1e6b9b2c0
Thiago Saraiva <thiagofreitassaraiva@yahoo.com.br>
2023-06-03 00:51:38 -0300

    Chess layer and printing the board

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,24 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece{
+	
+	private Color color;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+	
+	/*Retirando o set pois a cor não pode ser modificada, ele só pode ser acessada
+	public void setColor(Color color) {
+		this.color = color;
+	}*/	
+
+}
